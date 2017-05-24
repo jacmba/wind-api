07ddc93a75e24c8915a3e85876c573e74ab16a37
Jacinto Mba Cantero <j.mba@kentech-sp.com>
2017-05-24 09:24:41 +0100

    Server entry point

--- src/index.js
+++ src/index.js
@@ -5,4 +5,28 @@
 
 'use strict';
 
-console.log('Hello wind!');
+// Declare dependencies
+let Express = require('express');
+let XwindCtrl = require('./xwind/routes');
+
+// Instantiate objects
+let app = Express();
+let xCrtl = new XwindCtrl();
+
+// Setup routes
+app.get('/xwind', (req, res) => {
+  xCrtl.getXwind(req, res);
+});
+
+// Setup server port
+const PORT = process.env.PORT || 3000;
+
+// Startup server
+app.listen(PORT, (err) => {
+  if(err) {
+    console.error(`Error starting server: ${err}`);
+    process.exit(1);
+  }
+
+  console.log(`Server listening on port ${PORT}`);
+});
